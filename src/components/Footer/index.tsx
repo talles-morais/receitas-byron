e9d7f57098af67ed00fa1746fa4af8878b420455
Talles Alves <talves2503@gmail.com>
2025-06-12 00:26:01 -0300

    feat: estrutura do footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        <p>© 2025 Receitas Deliciosas. Todos os direitos reservados</p>
+      </div>
+    </footer>
+  )
+}
