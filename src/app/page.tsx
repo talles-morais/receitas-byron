f0f3298b3e274388017a001a58aa0beaaa56a8cb
Talles Alves <talves2503@gmail.com>
2025-06-12 14:21:45 -0300

    chore: TODO para os cards de receita

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
         <div className="flex flex-col items-center container mx-auto">
           <h2 className="text-lg font-bold">Receitas em destaque</h2>
 
-          {/* cards de receita */}
+          {/* TODO: cards de receita */}
 
           <Link className="flex text-orange-400 hover:text-orange-700 transition-colors" href="/receitas">
             Ver todas as receitas
