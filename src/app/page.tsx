7c51bcff62c6fe3b221b13ee2f867ff522f1cae6
Talles Alves <talves2503@gmail.com>
2025-06-12 00:40:22 -0300

    feat: estrutura da seção Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex-grow">
-      <h1>Tela inicial</h1>
+      <div className="container mx-auto">
+        {/* seção Hero */}
+        <section>
+          <h1>Receitas deliciosas</h1>
+          <p>Descubra receitas simples e saborosas para todas as ocasiões</p>
+
+          <Link href="/receitas">
+            Ver todas as receitas 
+          </Link>
+        </section>
+      </div>
     </main>
   );
 }
