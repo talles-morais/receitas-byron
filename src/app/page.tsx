50b65e157de5a72c736f205a050cee6c52657966
Talles Alves <talves2503@gmail.com>
2025-06-12 01:08:28 -0300

    feat: adiciona estilização da secao receitas em destaque

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="flex-grow">
 
       {/* seção Hero */}
-      <section className=" bg-orange-50 py-12">
+      <section className="bg-orange-50 py-12">
         <div className="flex flex-col gap-6 items-center container mx-auto">
           <h1 className="text-5xl font-bold">Receitas deliciosas</h1>
           <p className="text-xl">Descubra receitas simples e saborosas para todas as ocasiões</p>
@@ -18,13 +18,13 @@ export default function Home() {
       </section>
 
       {/* seção receitas em destaque */}
-      <section>
-        <div className="container mx-auto">
-          <h2>Receitas em destaque</h2>
+      <section className="py-12">
+        <div className="flex flex-col items-center container mx-auto">
+          <h2 className="text-lg font-bold">Receitas em destaque</h2>
 
           {/* cards de receita */}
 
-          <Link href="/receitas">
+          <Link className="flex text-orange-400 hover:text-orange-700 transition-colors" href="/receitas">
             Ver todas as receitas
             <ChevronRight />
           </Link>
