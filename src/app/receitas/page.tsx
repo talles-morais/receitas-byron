4f4467004450f05c10c55dcfafe8723af9a75182
Talles Alves <talves2503@gmail.com>
2025-06-13 23:16:44 -0300

    feat: estilo da pagina de receitas

--- src/app/receitas/page.tsx
+++ src/app/receitas/page.tsx
@@ -2,16 +2,18 @@ import { recipes } from "@/lib/data"
 
 export default function ReceitasPage() {
   return (
-    <main>
-      <h1>Todas as receitas</h1>
+    <main className="flex-grow">
+      <div className="container mx-auto py-8">
+        <h1 className="text-3xl font-bold">Todas as receitas</h1>
 
-      {/* cards */}
-      <div>
-        { recipes.map((recipe) => (
-          <div>
-            {/* card da receita */}
-          </div>
-        )) }
+        {/* cards */}
+        <div>
+          {recipes.map((recipe) => (
+            <div>
+              {/* card da receita */}
+            </div>
+          ))}
+        </div>
       </div>
     </main>
   )
