9ad335e50065fb50628dd61c013fd6d1fb690c95
Talles Alves <talves2503@gmail.com>
2025-06-14 00:09:20 -0300

    feat: estrutura da pagina receitas

---
+++ src/app/receitas/page.tsx
@@ -0,0 +1,13 @@
+export default function ReceitasPage() {
+  return (
+    <main className="flex-grow py-8">
+      <div className="container mx-auto">
+        <h1 className="text-3xl font-bold">Todas as receitas</h1>
+
+        <div>
+          {/* array de cards de receitas */}
+        </div>
+      </div>
+    </main>
+  )
+}
