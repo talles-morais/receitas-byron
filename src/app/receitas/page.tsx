e436f3cfd04f1f1045dbff00bcc1f547682b9606
Talles Alves <talves2503@gmail.com>
2025-06-14 00:21:02 -0300

    feat: cards de receita

--- src/app/receitas/page.tsx
+++ src/app/receitas/page.tsx
@@ -1,11 +1,16 @@
+import RecipeCard from "@/components/RecipeCard";
+import { recipes } from "@/lib/data";
+
 export default function ReceitasPage() {
   return (
     <main className="flex-grow py-8">
       <div className="container mx-auto">
         <h1 className="text-3xl font-bold">Todas as receitas</h1>
 
-        <div>
-          {/* array de cards de receitas */}
+        <div className="grid grid-cols-3 gap-8 mt-8">
+          {recipes.map((recipe) => (
+            <RecipeCard />
+          ))}
         </div>
       </div>
     </main>
