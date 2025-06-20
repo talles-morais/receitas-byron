87813eb0a23461447b331b39e049c6e9a73ab9a3
Talles Alves <talves2503@gmail.com>
2025-06-20 20:10:43 -0300

    feat: abrir e fechar modal de receita

--- src/app/receitas/page.tsx
+++ src/app/receitas/page.tsx
@@ -1,18 +1,34 @@
+"use client"
+
 import RecipeCard from "@/components/RecipeCard";
+import RecipeFormModal from "@/components/RecipeFormModal";
 import { recipes } from "@/lib/data";
+import { Plus } from "lucide-react";
+import { useState } from "react";
 
 export default function ReceitasPage() {
+  const [isRecipeModalOpen, setIsRecipeModalOpen] = useState(false)
+
   return (
     <main className="flex-grow py-8">
       <div className="container mx-auto">
-        <h1 className="text-3xl font-bold">Todas as receitas</h1>
+        <div className="flex justify-between w-full">
+          <h1 className="text-3xl font-bold">Todas as receitas</h1>
+
+          <button onClick={() => setIsRecipeModalOpen(true)} className="flex items-center gap-2 px-4 py-2 rounded-lg text-white bg-black hover:bg-gray-800 transition-colors">
+            <Plus size={16} />
+            Nova receita
+          </button>
+        </div>
 
         <div className="grid grid-cols-3 gap-8 mt-8">
           {recipes.map((recipe) => (
-            <RecipeCard key={recipe.id} recipe={recipe}/>
+            <RecipeCard key={recipe.id} recipe={recipe} />
           ))}
         </div>
       </div>
+
+      <RecipeFormModal isOpen={isRecipeModalOpen} onClose={() => setIsRecipeModalOpen(false)} />
     </main>
   )
 }
